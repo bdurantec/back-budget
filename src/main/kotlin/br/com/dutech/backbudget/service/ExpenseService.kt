da8d8cafc4172b20877cb4c1fb3e6609c10bd3e7
Bruno Durante <brunodurantec@gmail.com>
2022-08-12 12:22:55 -0300

    Inserindo endpoint de lista de despesas e sua estrutura basica

---
+++ src/main/kotlin/br/com/dutech/backbudget/service/ExpenseService.kt
@@ -0,0 +1,32 @@
+package br.com.dutech.backbudget.service
+
+import br.com.dutech.backbudget.model.Expense
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+import java.util.*
+
+@Service
+class ExpenseService(private var expenses: List<Expense>) {
+
+    init {
+        val expense1 = Expense(
+            id = 1,
+            description = "Supermarket",
+            value = 300.00,
+            date = LocalDateTime.now()
+        )
+        val expense2 = Expense(
+            id = 2,
+            description = "Restaurant",
+            value = 100.00,
+            date = LocalDateTime.now()
+        )
+
+        expenses = Arrays.asList(expense1, expense2)
+    }
+
+    fun getExpenseList(): List<Expense> {
+        return expenses
+    }
+
+}
